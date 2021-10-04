11fda8285646f539567680c93bf07b83764c06e0
d4rkstrife <p.gdc85@gmail.com>
2021-10-04 21:21:15 +0200

    array modification

--- src/DataFixtures/FiguresFixtures.php
+++ src/DataFixtures/FiguresFixtures.php
@@ -13,43 +13,50 @@ class FiguresFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $datas = array(
-            array(
+        $datas = [
+            [
                 "nom" => "mute",
                 "description" => "Saisie de la carre frontside de la planche entre les deux pieds avec la main avant.",
                 "groupe" => "grab"
-            ),
-            array(
+            ],
+
+            [
                 "nom" => "melancholie",
                 "description" => "Saisie de la carre backside de la planche, entre les deux pieds, avec la main avant.",
                 "groupe" => "grab"
-            ),
-            array(
+            ],
+
+            [
                 "nom" => "indy",
                 "description" => "Saisie de la carre frontside de la planche, entre les deux pieds, avec la main arrière.",
                 "groupe" => "grab"
-            ),
-            array(
+            ],
+
+            [
                 "nom" => "360",
                 "description" => "Trois six pour un tour complet.",
                 "groupe" => "rotation"
-            ),
-            array(
+            ],
+
+            [
                 "nom" => "stalefish",
                 "description" => "Saisie de la carre backside de la planche entre les deux pieds avec la main arrière.",
                 "groupe" => "grab"
-            ),
-            array(
+            ],
+
+            [
                 "nom" => "tail grab",
                 "description" => "Saisie de la partie arrière de la planche, avec la main arrière.",
                 "groupe" => "grab"
-            ),
-            array(
+            ],
+
+            [
                 "nom" => "720",
                 "description" => "Sept deux pour deux tours complets.",
                 "groupe" => "rotation"
-            )
-        );
+            ]
+
+        ];
         foreach ($datas as $data) {
             $article = new Figure();
             $article->setName($data["nom"]);
