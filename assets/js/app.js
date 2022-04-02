febfad9bb34c85f6186712549e8af1b289f040e4
d4rkstrife <p.gdc85@gmail.com>
2022-04-02 07:55:57 +0200

    password message

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 const showElt = () => {
     document.querySelector('.see-media-button').style.display = "none";
     document.querySelector('.trick-page-mid').style.display = "flex";
-
+    document.querySelector('.pictures_container').style.display = "flex";
+    console.log(elt);
 }
 const getButton = () => {
     const buttonShow = document.querySelector('.see-media-button');
